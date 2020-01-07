bc1eda95a7115000524d537c5cbbfd2e8590e3f1
bindu <bmahadevostb2@gmail.com>
2020-01-07 17:38:08 +0530

    commit3

--- src/Components/LeaveForm/LeaveForm.js
+++ src/Components/LeaveForm/LeaveForm.js
@@ -6,63 +6,59 @@ import Navbar from '../Navbar/Navbar';
 class LeaveForm extends Component {
   render() {
     return (
-      <div>
+      <div className="leaveform">
 
       <div>
         <Navbar/>
       </div><br></br>
 
-      <h3 className="leaveapp">Leave Application</h3>
-      <p className="sidebar">Attendance Regularization</p>
-      <p className="sidebar">Help Desk</p>
-      <p  className="sidebar">Leaves</p>
-      <form className="container4">
-
-       <label>Leave Type  </label>
-       <select className="leaves">
+      <h3 className="leaveapp">Leave Application</h3><br></br>
+      {/* <p className="sidebar">Attendance Regularization</p>
+      <p  className="sidebar">Leaves</p> */}
+      <form>
+      <span>Leave Type</span>
+      <span><select className="leaves">
        <option></option>
        <option value="Earned Leave">Earned Leave</option>
        <option value="Componsentory Off">Componsentory Off</option>
        <option value="Sick Leave">Sick Leave</option>
-       </select><br></br>
+       </select></span><br></br><br></br>
+       <div className="grid-container">
+    
+       <label>From Data</label>
+       <input type="text" name=" Date" placeholder=" "></input>
       
-       <div>
-       <label>From Date  </label>
-       <input className="num" type="text" name=" Date" placeholder=" "></input></div>
-         
-       <label>From Session  </label>
-       <select className="session">
-       <option>Session1</option>
-       <option>Session2</option>
-       </select><br></br>
+       <label>To Date </label>
+       <input type="number" name=" Date" placeholder=" "></input><br></br>
 
-       <label className="date">To Date : </label>
-       <input className="num1" type="number" name=" Date" placeholder=" "></input>
-
-       <label className="session2">To Session  </label>
-       <select className="option">
+       {/* {<label>To Session  </label>
+       <select>
        <option>Session2</option>
        <option>Session1</option>
-       </select><br></br>
+       </select>
+      <br></br> */} 
 
        <label>Days</label>
        <input type="number" name=" Days" placeholder="0"></input>
 
        <label>Balance</label>
-       <input type="number" name=" Balance" placeholder="0"></input><br></br>
+       <input type="number" name=" Balance" placeholder="0"></input>
+       </div><br></br>
 
+       <div>
        <label>Reason</label>
-       <input type="text" name=" Reason" placeholder=""></input><br></br>
+       <input className="reason" type="text" name=" Reason" placeholder=""></input><br></br>
+       </div>
 
        <label>Contact Details</label>
-       <input type="text" name=" Contact Details" placeholder=" "></input><br></br>
+       <input className="contact" type="text" name=" Contact Details" placeholder=" "></input><br></br>
 
        <label>CC To</label>
-       <input type="text" name=" CC" placeholder=""></input>
+       <input className="holder5" type="text" name=" CC" placeholder=""></input><br></br>
 
-       <p>Note : </p>
-       <button>Apply</button>
-       <button>Cancel</button>
+       <p id="note">Note : Seperate multiple emails with commas </p>
+       <button className="apply">Apply</button><br></br>
+       <button id="cancelbtn">Cancel</button>
      </form>
        <div>
          <Footer/>
@@ -74,5 +70,3 @@ class LeaveForm extends Component {
 }
 
 export default LeaveForm;
-
-
--- src/Components/LeaveSummary/LeaveSummary.js
+++ src/Components/LeaveSummary/LeaveSummary.js
@@ -11,41 +11,46 @@ class LeaveSummary extends Component {
                <div>
                  <Navbar/>
                  </div><br></br>
-          <div className="row">
-                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 container5">
+              <div className="row">
+                <div className="container5">
                   <div id="leavebal">Leave Balance-2020</div><br></br>
                   <div className="leaveatt">
-                  <div>WFH</div>
-                  <div>0</div>
-                  <div>ML</div>
-                  <div>0</div>
-                  <div>EL</div>
-                  <div>1</div>
-                  <div>CO</div>
-                  <div>0</div>
-                  <div>SL</div>
-                  <div>0.5</div>
+                  <div id="contents">WFH</div>
+                  <div id="contents">0</div>
+                  <div id="contents4">ML</div>
+                  <div id="contents4">0</div>
+                  <div id="contents">EL</div>
+                  <div id="contents">1</div>
+                  <div id="contents4">CO</div>
+                  <div id="contents4">0</div>
+                  <div id="contents">SL</div>
+                  <div id="contents">0.5</div>
+                  <div class="leavebal">View Leave summary</div>
                  </div>
                 </div> 
 
-                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 container5">
-                  <div id="leavebal">Leave Balance-2020</div><br></br>
-                  <div className="leaveatt">
-                  <div>WFH</div>
-                  <div>0</div>
-                  <div>ML</div>
-                  <div>0</div>
-                  <div>EL</div>
-                  <div>1</div>
-                  <div>CO</div>
-                  <div>0</div>
-                  <div>SL</div>
-                  <div>0.5</div>
+                <div className="container6">
+                <div id="leavebal1">Upcoming Holidays</div><br></br>
+                  <div className="leaveatt1">
+                  <div className="para6">Wednesday  15-01-2020</div>
+                  <div className="para7">Sankranthi</div>
+                 </div>
+                </div>
+
+                <div className="container7">
+                  <div id="leavebal6">Leave Balance-2020</div><br></br>
+                  <div className="leaveatt6">
+                  <div id="contents1">EL</div>
+                  <div id="contents1">19 Dec 2019 am not feeling well</div>
+                  <div id="contents1">EL</div>
+                  <div id="contents1">20 Dec 2019 Have personal work</div>
+                  <div id="contents1">EL</div>
+                  <div id="contents1">27 Dec 2019 am not feeling well</div>
                  </div>
                 </div> 
-                </div>
-              <div>
-            
+
+              </div>
+              <div id="footer2">
                 <Footer/>
               </div>
             </div>
